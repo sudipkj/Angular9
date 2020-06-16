37c83268a8e938f2d41a153862d80ff8c5be25fd
Sudip <sudipkj@gmail.com>
2020-06-16 13:04:50 +0300

    added material for buttons

--- Full Angular 8 Practice backup/angular9Demo/src/app/emp-pipe.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { EmpPipePipe } from './emp-pipe.pipe';
-
-describe('EmpPipePipe', () => {
-  it('create an instance', () => {
-    const pipe = new EmpPipePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
